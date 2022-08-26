eb25fb787db97e379a54c685a399a1b1e5a5a26a
Alessia <a.amore9393@gmail.com>
2022-08-26 15:36:00 +0200

    calculating is working - clear not working yet

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
 "use strict";
+
+window.addEventListener("load", init);
+
+const resultList = document.querySelector("#results");
+const clearButton = document.querySelector("#clear");
+const calculateButton = document.querySelector("#calculate");
+let resultRounded;
+let result;
+
+function init() {
+  calculateButton.addEventListener("click", calculating);
+  clearButton.addEventListener("click", clearFields);
+}
+function clearFields() {
+  firstString = "";
+  secondString = "";
+  document.querySelector;
+}
+function calculating() {
+  //reading operator and input strings
+  const operator = document.getElementById("operator").value;
+  const firstString = document.querySelector("#firstnumber").value;
+  const secondString = document.querySelector("#secondnumber").value;
+  //convert strings into numbers
+  const firstNumber = Number.parseFloat(firstString);
+  const secondNumber = Number.parseFloat(secondString);
+  if (operator === "add") {
+    result = firstNumber + secondNumber;
+  }
+  if (operator === "sub") {
+    result = firstNumber - secondNumber;
+  }
+  if (operator === "mul") {
+    result = firstNumber * secondNumber;
+  }
+  if (operator === "div") {
+    result = firstNumber / secondNumber;
+  }
+  //giving the first number, the latest result number
+  document.getElementById("firstnumber").value = result;
+
+  //reading if the box is checked
+  const rounding = document.querySelector("#doround").checked;
+  //rounding
+  if (rounding) {
+    const decimals = document.getElementById("decimals").value;
+    console.log(decimals);
+    resultRounded = result.toFixed(decimals);
+    console.log("result", result);
+    appendRounded();
+  } else {
+    appendNumber();
+  }
+}
+//appending
+function appendNumber() {
+  let li = document.createElement("li");
+  li.textContent = result;
+  resultList.append(li);
+  resultList.scrollTo(0, 1000000);
+}
+
+function appendRounded() {
+  let li = document.createElement("li");
+  li.textContent = resultRounded;
+  resultList.append(li);
+  resultList.scrollTo(0, 1000000);
+}
